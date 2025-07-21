cba3279c73a53c9705bffe238ef47ed345ae06dd
nielsemb <martinbonielsen.astro@gmail.com>
2025-07-21 13:20:39 +0100

    safety patch for extremely binned time series

--- pbjam/IO.py
+++ pbjam/IO.py
@@ -301,8 +301,9 @@ class psd():
         if width is None:
             width = 100*df
 
-        freq_cen = 0.5*self.Nyquist
-
+        # freq_cen = 0.5*self.Nyquist
+        freq_cen = max([0.5*self.Nyquist, width + df])
+        
         Nfreq = int(oversampling*width/df)
 
         freq = freq_cen + (df/oversampling) * np.arange(-Nfreq, Nfreq, 1)
