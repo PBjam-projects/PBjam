60742eba431a4274a5acaf67db869ccfc4199d90
Joel Ong <joel@hyad.es>
2025-11-13 14:22:29 +1100

    separate out creating and running l20model (to admit e.g. intermediate step of manual edits for priors)

--- pbjam/modeID.py
+++ pbjam/modeID.py
@@ -65,7 +65,7 @@ class modeID(plotting, ):
         if self.priorPath is None:
             self.priorPath = IO._getPriorPath()
  
-    def runl20model(self, progress=True, dynamic=False, minSamples=5000, sampler_kwargs={}, logl_kwargs={}, PCAsamples=50, PCAdims=6, **kwargs):
+    def makel20model(self, PCAsamples=50, PCAdims=6, **kwargs):
         """
         Runs the l20 model on the selected spectrum.
 
@@ -91,9 +91,7 @@ class modeID(plotting, ):
         """
 
         f = self.f[self.sel]
-
         s = self.s[self.sel]
-
         self.l20model = Asyl20model(f, s, 
                                     self.obs, 
                                     self.addPriors, 
@@ -101,6 +99,35 @@ class modeID(plotting, ):
                                     PCAsamples, 
                                     PCAdims,
                                     priorPath=self.priorPath)
+    
+
+    def runl20model(self, progress=True, dynamic=False, minSamples=5000, sampler_kwargs={}, logl_kwargs={}, PCAsamples=50, PCAdims=6, **kwargs):
+        """
+        Runs the l20 model on the selected spectrum.
+
+        Parameters
+        ----------
+        progress : bool, optional
+            Whether to show progress during the model run. Default is True.
+        dynamic : bool, optional
+            Whether to use dynamic nested sampling. Default is False (static nested sampling).
+        minSamples : int, optional
+            The minimum number of samples to generate. Default is 5000.
+        logl_kwargs : dict, optional
+            Additional keyword arguments for the log-likelihood function. Default is an empty dictionary.
+        PCAsamples : int, optional
+            Number of samples for PCA. Default is 50.
+        PCAdims : int, optional
+            Number of dimensions for PCA. Default is 6.
+
+        Returns
+        -------
+        result : dict
+            Parsed results from the l20 model.
+        """
+
+        if not hasattr(self, 'l20model'):
+            self.makel20model(PCAsamples=PCAsamples, PCAdims=PCAdims)
         
         self.l20Samples = self.l20model.runSampler(progress=progress,
                                                    dynamic=dynamic,
